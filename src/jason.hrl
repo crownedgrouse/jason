30300e2f3895ec2ab48e2a31af83e1f04d2290ae
crownedgrouse <eric.pailleau@wanadoo.fr>
2025-04-12 11:16:25 +0200

    Add include for .hrl

---
+++ src/jason.hrl
@@ -0,0 +1,7 @@
+-record(opt, {nl      = []     :: list()
+             ,indent  = false
+             ,records = []     :: list()
+             ,mode    = struct :: atom()
+             ,binary  = undefined :: atom()
+             ,aliases = []     :: list()
+             }).
